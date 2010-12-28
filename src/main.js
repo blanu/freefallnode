240000045e793d37e758432fabb7bfd7c7eb7855
Brandon Wiley <brandon@blanu.net>
2010-12-28 12:29:59 -0600

    Initial import of demo

---
+++ src/main.js
@@ -0,0 +1,40 @@
+var Request = require("ringo/webapp/request").Request,
+    Message = require("./message").Message;
+
+exports.app = function (request) {
+    return exports[request.method](request);
+}
+
+exports.GET = function (request) {
+    var messages = Message.all().order("created").fetch(),
+        body = '<html><head><link rel="stylesheet" media="screen" href="/screen.css" type="text/css" /></head><body><h1>Guest book</h1>';
+    
+    if (messages.length > 0) {
+        messages.forEach(function (m) {
+            body += '<div class="message"><h3>' + m.author + ':</h3><p>' + m.content + '</p>' + m.created + '</div>';
+        });
+    } else {
+        body += '<div class="message">No messages, add the first!</div>';
+    }
+
+    body += '<form method="POST"><p><label>Author</label><input type="text" name="author" /></p><p><label>Message</label><textarea name="content"></textarea></p><p><button type="submit">Send</button></p></form><br/><br/>' + new Date() + '</body></html>';
+
+    return {
+        status: 200,
+        headers: {"Content-Type": "text/html"},
+        body: [body]
+    }
+}
+
+exports.POST = function (request) {
+    var params = new Request(request).params,
+        msg = new Message(params);
+
+    msg.put();
+    
+    return {
+        status: 303,
+        headers: {Location: "/"},
+        body: ["See other: /"]
+    };
+}
