5ac781680c0513d3cdc9fef3a13711e96dd7bb19
Brandon Wiley <brandon@blanu.net>
2011-01-03 15:08:15 -0600

    Added GET for easier testing

--- js/testView.js
+++ js/testView.js
@@ -5,6 +5,17 @@ exports.app = function (request) {
     return exports[request.method](request);
 }
 
+exports.GET = function (request) {
+    logging.debug("view get...");
+    var params = new Request(request).params
+
+    return {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: ["[1,2,3,7,8,9]"]
+    };
+}
+
 exports.POST = function (request) {
     logging.debug("view post...");
     var params = new Request(request).params
